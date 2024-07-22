8b8f4b6f87d54fae759be042864380afd3c2ce10
Rudy Wang <a86742902@gmail.com>
2024-07-22 16:49:55 +0800

    doc: 修改了main.dart的code，使其能夠正確初始化

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:flutter_mvvm_demo/page/home/home.dart';
 import 'package:flutter_mvvm_demo/viewModels/initialize/initialize_view_model.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  InitializeViewModel initializeViewModel = InitializeViewModel();
+  await initializeViewModel.init();
+
   runApp(ChangeNotifierProvider(
-      create: (_) => InitializeViewModel(),
+      create: (_) => initializeViewModel,
       child: const MaterialApp(
         home: HomePage(),
       )));
